9bdfb2d0958a8902e2803a2b9e34e830fbf228e1
RichardZetterberg <48049445+RichardZetterberg@users.noreply.github.com>
2019-06-29 20:14:53 +0300

    Add files via upload

--- http_proxy2.js
+++ http_proxy2.js
@@ -1,27 +1,54 @@
 var http = require('http');
+var tls = require('tls');
+var url = require('url');
 
-http.createServer(onRequest).listen(3000);
+http.createServer(function (request, response){
+	var elect = url.parse(request.url);
+	if (elect.hostname == 'http'){
+		console.log('serve: ' + request.url);
 
-function onRequest(request, response) {
-  console.log('serve: ' + request.url);
+ 		var options = {
+  	 	hostname: 'www.google.com',
+    	port: 80,
+    	path: request.url,
+    	method: request.method,
+    	headers: request.headers
+  	};
 
-  var options = {
-    hostname: 'www.google.com',
-    port: 80,
-    path: request.url,
-    method: request.method,
-    headers: request.headers
-  };
+  	var proxy = http.request(options, function (res) {
+    	response.writeHead(res.statusCode, res.headers)
 
-  var proxy = http.request(options, function (res) {
-    response.writeHead(res.statusCode, res.headers)
+    	res.pipe(response, {
+      			end: true
+    		});
+  		});
 
-    res.pipe(response, {
-      end: true
-    });
-  });
+  	request.pipe(proxy, {
+    	end: true
+  	});
+	}else if (elect.hostname == 'https'){
+		var request = Http.request({
+   			host: '192.168.5.8',
+   			port: 3128,
+    		method: 'CONNECT',
+    		path: 'twitter.com:443',
+		});
 
-  request.pipe(proxy, {
-    end: true
-  });
-}
+		request.on('connect', function (res, socket, head) {
+    	var cts = tls.connect({
+   			host: 'twitter.com',
+    		socket: socket
+    	}, function () {
+        	cts.write('GET / HTTP/1.1rnHost: twitter.comrnrn');
+    	});
+
+    	cts.on('data', function (data) {
+        	console.log(data.toString());
+    	});
+	});
+
+		req.end();
+	}else console.log('Error. Something went wrong!!!');
+}).listen(3000);
+
+
