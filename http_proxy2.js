4f3f6e552e7ad6db01e98844a254a5aeb6026f64
RichardZetterberg <48049445+RichardZetterberg@users.noreply.github.com>
2019-07-01 20:38:09 +0300

    Update http_proxy2.js

--- http_proxy2.js
+++ http_proxy2.js
@@ -1,25 +1,25 @@
 //connecting main modules
 var http = require('http');
-var tls = require('tls');
+var net = require('net');
 var url = require('url');
 
 http.createServer(function (request, response){//create server listened on port 3000
 	
 	var elect = url.parse(request.url);//parse the address bar 
+	//object of options 
+ 	var options = { 	
+   		hostname: elect.hostname,
+    		port: 80,
+    		path: request.url,
+    		method: request.method,
+    		headers: request.headers
+ 	};
+	
 	
 	if (elect.hostname == 'http'){//choose between 'http' and 'https' functions. If another then catch mistake 
 		
-		console.log('serve: ' + request.url);//log reference on requested object
+	console.log('serve: ' + request.url);//log reference on requested object
 		
-		//object of options 
- 		var options = { 	
-  	 		hostname: 'www.google.com',
-    			port: 80,
-    			path: request.url,
-    			method: request.method,
-    			headers: request.headers
-  		};
-	
 	//reproduce the query
   	var proxy = http.request(options, function (res) {
     	response.writeHead(res.statusCode, res.headers)
@@ -36,33 +36,56 @@ http.createServer(function (request, response){//create server listened on port 
 	
 	//checking for hostname 'https'
 	}else if (elect.hostname == 'https'){
-		var request = Http.request({
-   			host: '192.168.5.8',
-   			port: 3128,
-    			method: 'CONNECT',
-    			path: 'twitter.com:443',
-		});
+    		var proxyRequest = http.request(options);
 
-		
-		
-		
-		request.on('connect', function (res, socket, head) {
-			//accepts an options argument which contains the socket we received from the connect event handler.
-    			var cts = tls.connect({
-   				host: 'twitter.com',
-    				socket: socket
-    			}, function () {
-        			cts.write('GET / HTTP/1.1rnHost: twitter.comrnrn');
+    		proxyRequest.on('response', function (proxyResponse) {
+        	response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+			
+        	proxyResponse.on('data', function (chunk) {
+           		response.write(chunk, 'binary');
+        	});
+			
+        	proxyResponse.on('end', function () {
+            		response.end();
+        	});
     	});
 
-			
-    		cts.on('data', function (data) {//show data
-        		console.log(data.toString());
-    		});
-	});
+    	request.on('data', function (chunk) {
+        	proxyRequest.write(chunk, 'binary');
+   	});
+    	request.on('end', function () {
+       		proxyRequest.end();
+    	});
 
-		req.end();
-		
+});
+
+	server.on('connect', function (request, socketRequest, head) {
+
+    	var fromURL = url.parse('http://' + request.url);
+
+    	var socket = net.connect(fromURL.port, fromURL.hostname, function() {
+        	// Tell the client that the connection is established
+        	socket.write(head);
+        	socketRequest.write("HTTP/" + request.httpVersion + " 200 Connection established\r\n\r\n");
+   	})
+
+    	//Host Tunneling
+    	socket.on('data', function (chunk) {socketRequest.write(chunk);});
+    	socket.on('end', function () {socketRequest.end();});
+    	socket.on('error', function () {
+        //Tell the client that an error has occurred
+        socketRequest.write("HTTP/" + request.httpVersion + " 500 Connection error\r\n\r\n");
+        socketRequest.end();
+    	})
+
+    	//Client Tunneling
+    	socketRequest.on('data', function (chunk) {socket.write(chunk);});
+    	socketRequest.on('end', function () {socket.end();});
+    	socketRequest.on('error', function () {socket.end();});
+
+	})
+
+	server.listen(8080);//server listening on port: 8080
 		
 	//if anything else error on console	
 	}else console.log('Error. Something went wrong!!!');
